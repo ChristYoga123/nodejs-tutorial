6bbfa993d4fe337671842cdc4770dc675dc007bc
ChristYoga123 <christianuswibisono@gmail.com>
2023-02-05 13:47:24 +0700

    lesson 3

---
+++ 3_function/func.js
@@ -0,0 +1,16 @@
+/* FUNCTION WITHOUT PARAMETER */
+// define function
+function hello()
+{
+    console.log("Halo");
+}
+
+// call function
+hello();
+
+/* FUNCTION WITH PARAMETER */
+function greet(name) {
+    console.log(`Hai ${name}`);
+}
+
+greet("Gloria")
