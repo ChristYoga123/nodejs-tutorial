47e791e856df272fca540637fae3e3156970b18d
ChristYoga123 <christianuswibisono@gmail.com>
2023-02-05 13:50:49 +0700

    lesson 4

---
+++ 4_object_global/object_global.js
@@ -0,0 +1,7 @@
+// Object Global untuk mengetahui detail file sekarang
+console.log(__filename);
+
+// Object Global untuk mengeteahui detail folder sekarang
+console.log(__dirname);
+
+/* Fungsinya untuk meletakkan asset dari web app */
